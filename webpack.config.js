7372a404921a9c14cb7298e58b32466b0776a09b
Anton Pavelyev <pap_90@mail.ru>
2016-11-25 14:15:17 +0300

    minimal angular2 install

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,25 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        app: './app/app'
+        app: './app/main',
+        vendor: './app/vendor',
     },
     output: {
         path: 'dist',
         filename: '[name].bundle.js',
     },
-    plugins: [new HtmlWebpackPlugin()],
+    resolve: {
+        extensions: ['', '.js', '.ts']
+    },
+    module: {
+        loaders: [
+            { test: /\.ts/, loader: 'ts', exclude: /node_modules/ }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './app/index.html',
+            inject: 'body',
+        })],
     devtool: 'source-map'
 };
