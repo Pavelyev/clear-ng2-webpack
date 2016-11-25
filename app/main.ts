87142ef9e775f776d4124cd1ef10928678d6773f
Anton Pavelyev <pap_90@mail.ru>
2016-11-25 17:14:25 +0300

    hot reload main module

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,14 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+
+function main(){
+    platformBrowserDynamic().bootstrapModule(AppModule);
+}
+
+// can't find correct typings
+var am = module as any;
+if(am.hot){
+    am.hot.accept();
+    main();
+}
